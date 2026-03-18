4db0f676e25f6a535b15612da69955193de47e64
Joao Paulo Lordelo Pedreira Vivas <joao.vivas@avonale.local>
2026-03-18 12:56:34 -0300

    feat: Criação dos testes para email e senha invádos, nao formatados e em brancos

--- tests/fazerLogin.spec.ts
+++ tests/fazerLogin.spec.ts
@@ -2,11 +2,37 @@ import test from "@playwright/test";
 import PaginaLogin from "./PageObjectModel/PaginaLogin";
 
 test.describe("Pagina de Login", () => {
-    test("Usar email e senha valido", async ({page}) => {
-        const paginaLogin = new PaginaLogin(page);
 
-        await paginaLogin.visitar()
+    let paginaLogin : PaginaLogin;
 
+    test.beforeEach( async ({page}) => {
+        paginaLogin = new PaginaLogin(page)
+        paginaLogin.visitar()
+    })
+
+    test("Usar email e senha valido", async () => {
         await paginaLogin.realizarLogin("guilherme@email.com","123456")
+
+        await paginaLogin.validarLogin()
+    })
+
+    test("Usar email e senha inválidos", async () => {
+        await paginaLogin.realizarLogin("email@teste.com","111111");
+
+        await paginaLogin.verificaMensagemErro("Você não está autorizado a acessar este recurso");
+    })
+
+    test("Usar email fora da formatação correta", async () =>{
+        await paginaLogin.preencherCamposLogin("email","1111111")
+
+        await paginaLogin.verificaMensagemErro("E-mail inválido")
+    })
+
+    test("Deixar um dos campos em branco", async () => {
+        await paginaLogin.preencherCamposLogin("","");
+
+        await paginaLogin.verificaMensagemErro("E-mail é obrigatório")
+        await paginaLogin.verificaMensagemErro("Senha é obrigatória")
+        
     })
 })
